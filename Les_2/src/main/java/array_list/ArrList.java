1c0ec6699898d58904d814f7cc0269fd2cc2ff14
kalldaglls <kalldaglls@mail.ru>
2023-02-23 18:58:57 +0300

    Les_2 commit

---
+++ Les_2/src/main/java/array_list/ArrList.java
@@ -0,0 +1,24 @@
+package array_list;
+
+public class ArrList implements GeekBrainsArray{
+    private int [] array;
+    private int size;
+
+    @Override
+    public void add(int a) {
+        if (array.length == 0){
+
+        }
+        array[0] = a;
+    }
+
+    @Override
+    public void remove(int a) {
+
+    }
+
+    @Override
+    public void find(int a) {
+
+    }
+}
